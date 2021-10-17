3b0362b54075eed5c0f351431ae51d54530ec8ac
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-10-17 12:01:39 +0100

    Removing an item

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -14,13 +14,22 @@ const Ingredients = () => {
     ])
   }
 
+  const removeIngredientHander = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    )
+  }
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHander} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHander}
+        />
       </section>
     </div>
   )
